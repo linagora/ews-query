848d4fb430ca86d942fba1146a6e03cecc329bc6
ddolcimascolo <ddolcimascolo@linagora.com>
2016-06-01 18:22:23 +0200

    Updated to use a patched node-ews.

--- index.js
+++ index.js
@@ -13,45 +13,50 @@ ews.ignoreSSL = true;
 ews.auth(options.username, options.password, options.host);
 
 ews.run('FindItem', {
-  attributes: {
-    Traversal: 'Shallow'
-  },
-  ItemShape: {
-    BaseShape: 'IdOnly',
-    AdditionalProperties: {
-      FieldURI: {
+  body: {
+    attributes: {
+      Traversal: 'Shallow'
+    },
+    ItemShape: {
+      BaseShape: 'IdOnly',
+      AdditionalProperties: {
+        FieldURI: {
+          attributes: {
+            FieldURI: 'item:Subject'
+          }
+        }
+      }
+    },
+    CalendarView: {
+      attributes: {
+        MaxEntriesReturned: 10,
+        StartDate: '2016-01-01T00:00:00Z',
+        EndDate: '2016-12-31T00:00:00Z'
+      }
+    },
+    ParentFolderIds: {
+      DistinguishedFolderId: {
         attributes: {
-          FieldURI: 'item:Subject'
+          Id: 'calendar'
         }
       }
     }
   },
-  CalendarView: {
-    attributes: {
-      MaxEntriesReturned: 10,
-      StartDate: '2016-01-01T00:00:00Z',
-      EndDate: '2016-12-31T00:00:00Z'
-    }
-  },
-  ParentFolderIds: {
-    DistinguishedFolderId: {
-      attributes: {
-        Id: 'calendar'
+  headers: {
+    'http://schemas.microsoft.com/exchange/services/2006/types': [{
+      RequestServerVersion: {
+        attributes: {
+          Version: 'Exchange2010'
+        }
+      },
+      ExchangeImpersonation: {
+        ConnectingSID: {
+          SmtpAddress: options.target
+        }
       }
-    }
+    }]
   }
-}, [{
-  RequestServerVersion: {
-    attributes: {
-      Version: 'Exchange2010'
-    }
-  },
-  ExchangeImpersonation: {
-    ConnectingSID: {
-      SmtpAddress: options.target
-    }
-  }
-}], function(err, result) {
+}, function(err, result) {
   console.log(util.inspect(err));
   console.log(util.inspect(result, false, null));
 });
